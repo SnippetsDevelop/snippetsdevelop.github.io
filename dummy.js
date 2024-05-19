7ae8a8565ec9703f5e69eb4852fb9ac02306265b
Avinash Vytlagit <avinashvytla351@gmail.com>
2024-05-20 01:05:58 +0530

    Responsive Hamburger Menu

---
+++ dummy.js
@@ -0,0 +1,8 @@
+/*let container = document.createElement("div");
+let menu = document.querySelector(".menu");
+container.appendChild(menu);
+container.className = "container";
+let dummy = document.createElement("div");
+dummy.className = "dummy";
+document.body.appendChild(container);
+document.body.appendChild(dummy);*/
